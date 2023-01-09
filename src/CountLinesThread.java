8929293e1878a3b2817aef40866ef62bf6f518b6
Aviost111 <Aviostroff@yahoo.com>
2023-01-09 15:58:13 +0200

    finish javadoc

--- src/CountLinesThread.java
+++ src/CountLinesThread.java
@@ -1,16 +1,27 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
+/***
+ * our class has two parameters ,filename that holds the file name and count that counts the lines.
+ */
 public class CountLinesThread extends Thread {
     private final String fileName;
     private int count;
 
+    /***
+     * Constructor
+     * @param fileName
+     */
     public CountLinesThread(String fileName) {
         this.fileName = fileName;
         this.count = 0;
     }
 
+    /***
+     * Our method implements the run method from the Runnable interface.
+     * it creates a bufferedReader for the file and keeps going to the
+     * next line and adds to count till there are no more lines and lastly closes the reader.
+     */
     @Override
     public void run() {
         try {
@@ -24,6 +35,10 @@ public class CountLinesThread extends Thread {
         }
     }
 
+    /***
+     * Getter for count
+     * @return returns count
+     */
     public int getCount() {
         return count;
     }
--- src/CountLinesThreadsPool.java
+++ src/CountLinesThreadsPool.java
@@ -3,15 +3,29 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.concurrent.Callable;
 
+/***
+ * our class has two parameters ,filename that holds the file name and count that counts the lines.
+ */
 public class CountLinesThreadsPool implements Callable<Integer> {
     private final String fileName;
     private int count;
 
+    /***
+     * Constructor
+     * @param fileName
+     */
     public CountLinesThreadsPool(String fileName) {
         this.fileName = fileName;
         this.count = 0;
     }
 
+    /***
+     * Our function implements the call method from the callable interface,
+     * it creates a bufferedReader for the file and keeps going to the
+     * next line and adds to count till there are no more lines.Lastly it closes the reader,and returns count.
+     * @return returns the count which holds the number of lines.
+     * @throws Exception
+     */
     @Override
     public Integer call() throws Exception {
         try {
